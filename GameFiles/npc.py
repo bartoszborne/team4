56c4e0408625c13ede85493926f84e0f6cc83e38
JANREY <janrey_mosuela@hotmail.co.uk>
2017-10-24 20:49:20 +0100

    finished. just add dialogue

--- GameFiles/npc.py
+++ GameFiles/npc.py
@@ -9,30 +9,19 @@ chief_kirill = {
     "dialogue": dialogues.d_chief_kirill_1,    
 }
 
-
+#- - - - - - - - - - - - -  HOSPITAL - - - - - - - - - - - - - - |
 
 
 hospital_receptionist = {
     "id": "nurse",
-    "name": "A nurse is at the reception desk",
-    "dialogue": {
-        "a": ["Hi do u wanna have some tea", "NPC response A.", [items.victim_belongings]],
-        "b": ["do u wanna f**k?", "NPC response B.",
-        
-            {"a": ["optiona 2", "NPC response a2.", "end_convo"],
-            "b": ["option b2", "NPC response b2.", 
-                {"a": ["option a3", "NPC response a3.", "end_convo"],
-                "b": ["option b3", "NPC response b3.", ""],
-                "c": ["option c3", "NPC response c3.", "end_convo"]}],
-            "c": ["option c2", "NPC response c2.", "end_convo"]}],
-        
-        "c": ["bye.", "NPC response C.", "end_convo"]
-    }
+    "name": "A nurse sitting at the reception desk is",
+    "dialogue": None, #replace me
+    
 }
 
 joe_branson = {
     "id": "joe",
-    "name": "Joe Branson",
+    "name": "Joe Branson is",
     "dialogue": {
         "a": ["A: Hey buddy, how you doin'?", "...", "end_convo"],
         "b": ["B: Does it hurt anywhere?", "...", "end_convo"],
@@ -40,7 +29,32 @@ joe_branson = {
     }
 }
 
+doctor_kim = {
+	"id": "doctor",
+	"name": "Dr. Kim is",
+	"dialogue": None, #replace me
+}
 
+killer = {
+	"id": "killer",
+	"name": "The Killer is",
+	"dialogue": None, #replace me
+}
+#- - - - - - - - - - - - -  SHIPPING COMPANY - - - - - - - - - - - - - - |
+
+shipping_ceo = {
+	"id": "CEO",
+	"name": "the Shipping CEO is",
+	"dialogue": None,	 #replace me
+}
+
+#- - - - - - - - - - - - -  JOE'S HOUSE - - - - - - - - - - - - - - |
+
+joe_wife = {
+	"id": "mrs branson",
+	"name": "Mrs Branson ",
+	"dialogue": None, #replace me
+}
 """Kirill
 
 Doctor

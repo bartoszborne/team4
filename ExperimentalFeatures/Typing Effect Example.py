ba97433b385b48a007126ebc119326c00ca1e1b9
Janrey <janrey_mosuela@hotmail.co.uk>
2017-10-25 17:08:35 +0100

    DIE

--- ExperimentalFeatures/Typing Effect Example.py
+++
@@ -1,9 +0,0 @@
-import sys
-from time import sleep
-
-# Reference: https://stackoverflow.com/questions/20302331/typing-effect-in-python (Accessed: 24/10/17)
-for char in words:
-    sleep(0.00001)
-    sys.stdout.write(char)
-    sys.stdout.flush()
-# End of Referenced material
